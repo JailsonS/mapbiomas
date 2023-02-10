99ae6ae04a4f9dc6c4099cbe3f36c521c908322a
JailsonS <engsoaresfilho@gmail.com>
2023-02-10 20:40:50 -0300

    update

---
+++ landsat_col_8/utils/util.py
@@ -0,0 +1,26 @@
+def shuffle(collection, seed=1):
+    """
+    Adds a column of deterministic pseudorandom numbers to a collection.
+    The range 0 (inclusive) to 1000000000 (exclusive).
+    """
+
+    collection = collection.randomColumn('random', seed)\
+        .sort('random', True)\
+        .map(lambda feature: feature.set(
+            'new_id',
+            ee.Number(feature.get('random')).multiply(1000000000).round()
+        )
+    )
+
+    # list of random ids
+    randomIdList = ee.List(
+        collection.reduceColumns(ee.Reducer.toList(), ['new_id'])
+        .get('list'))
+
+    # list of sequential ids
+    sequentialIdList = ee.List.sequence(1, collection.size())
+
+    # set new ids
+    shuffled = collection.remap(randomIdList, sequentialIdList, 'new_id')
+
+    return shuffled
