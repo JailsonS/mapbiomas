a7c4ca8dc187c70fa5989429a01f6cbb5e0be4b5
JailsonS <engsoaresfilho@gmail.com>
2023-03-01 23:23:09 -0300

    update repo

--- sentinel_col_beta/tests/test1/1_wetland_classify.py
+++ sentinel_col_beta/tests/test1/1_wetland_classify.py
@@ -1,4 +1,5 @@
-import ee, os
+import ee
+import os
 import numpy as np
 import pandas as pd
 import random
@@ -23,7 +24,8 @@ ASSET_OUTPUT = 'projects/imazon-simex/LULC/SENTINEL-2-CBETA/wetland'
 
 OUTPUT_VERSION = '1'
 
-FILEPATH_REF_AREA = os.path.abspath('./sentinel_col_beta/tests/test1/data/areas_c71_grid_sentinel.csv')
+FILEPATH_REF_AREA = os.path.abspath(
+    './sentinel_col_beta/tests/test1/data/areas_c71_grid_sentinel.csv')
 
 CLASS_VALUES = {
     3: 'forest formation',
@@ -54,23 +56,23 @@ PROPORTION_SAMPLES = pd.DataFrame([
     {'class':  1, 'min_samples': N_SAMPLES * 0.25, 'proportion': 0.25},
 ])
 
-TILES = ['21MTU', '21MUM', '21MUQ', '21MUP']
-    
+TILES = []
+
 YEARS = [
     # 2016,
     # 2017,
     # 2018,
-    # 2019,
-    2020, # <-- não rodar 2020
+    2019,
+    # 2020, # <-- não rodar 2020
     # 2021,
     # 2022,
 ]
 
 BANDS = ['B2', 'B3', 'B4', 'B8', 'B11', 'B12', 'QA60']
-NEW_BAND_NAMES = ['blue','green','red','nir','swir1','swir2', 'pixel_qa']
+NEW_BAND_NAMES = ['blue', 'green', 'red', 'nir', 'swir1', 'swir2', 'pixel_qa']
 
 FEAT_SPACE_BANDS = [
-    "gv", "gvs", "soil", "npv", "ndfi", "csfi", 
+    "gv", "gvs", "soil", "npv", "ndfi", "csfi",
     "ndfi_max", "ndfi_min",
     "soil_max", "soil_min",
     "gv_max", "gv_min",
@@ -88,54 +90,59 @@ RF_PARAMS = {
 '''
     Auxiliar Function
 '''
+
+
 def getProportionTable(table: pd.DataFrame, tile: str, year: int) -> pd.DataFrame:
-    
+
     tablePerc = table.query("year == {} & tile == '{}'".format(year, tile))
 
-
-    
     #tablePerc['area_ha'] = (tablePerc['area_ha']).astype(float).round(decimals=4)
-    tablePerc['area_percent'] = (tablePerc['area_ha'] / tablePerc['area_ha'].sum()).round(decimals=4)
-    tablePerc['n_samples'] = tablePerc['area_percent'].mul(N_SAMPLES).round().astype(int)
-
+    tablePerc['area_percent'] = (
+        tablePerc['area_ha'] / tablePerc['area_ha'].sum()).round(decimals=4)
+    tablePerc['n_samples'] = tablePerc['area_percent'].mul(
+        N_SAMPLES).round().astype(int)
 
     # join ref area with proportion
     df = pd.merge(tablePerc, PROPORTION_SAMPLES, how="outer", on="class")
-    
+
     # compare to min samples: rule (min_samples > n_samples = min_samples)
-    df.loc[df['min_samples'] > df['n_samples'], 'n_samples'] = df['min_samples']
+    df.loc[df['min_samples'] > df['n_samples'],
+           'n_samples'] = df['min_samples']
     df = df.replace(float("NaN"), 0)
 
     return df
 
+
 def getReferenceAreaTable(tile, year):
-   
-    referenceTable = pd.read_csv(FILEPATH_REF_AREA)[['tile', 'gridname','year','class', 'area_ha']]
+
+    referenceTable = pd.read_csv(FILEPATH_REF_AREA)[
+        ['tile', 'gridname', 'year', 'class', 'area_ha']]
 
     # normalize classes
     referenceTable = referenceTable.replace({'class': {
-        9:0, 30:0, 50:0, 19:0, 32:0, 20:0, 41:0, 11:0,
-        3: 0,4: 0,15: 0,12: 0,25: 0,33: 1,21: 0,
-        24: 0,39: 0,62: 0
-    }}).groupby(by=['tile', 'gridname', 'year','class']).sum().reset_index()
+        9: 0, 30: 0, 50: 0, 19: 0, 32: 0, 20: 0, 41: 0, 11: 0,
+        3: 0, 4: 0, 15: 0, 12: 0, 25: 0, 33: 1, 21: 0,
+        24: 0, 39: 0, 62: 0
+    }}).groupby(by=['tile', 'gridname', 'year', 'class']).sum().reset_index()
 
     referenceTable = getProportionTable(referenceTable, str(tile), int(year))
 
-    #pprint(referenceTable.head())
+    # pprint(referenceTable.head())
 
     return referenceTable
 
+
 def removeShadow(image):
-  threshShade = image.select('shade').gt(0.8)
-  threshNdfi = image.select('ndfi').gt(0.5)
-  #treshGv = image.select('ndvi').gt(0.5).and(image.select('ndfi').lt(0.4));
-  
-  mask = threshShade.And(threshNdfi)
-  
-  return image.mask(mask.eq(0))
+    threshShade = image.select('shade').gt(0.8)
+    threshNdfi = image.select('ndfi').gt(0.5)
+    # treshGv = image.select('ndvi').gt(0.5).and(image.select('ndfi').lt(0.4));
+
+    mask = threshShade.And(threshNdfi)
+
+    return image.mask(mask.eq(0))
+
 
 def trainRandomTileImg(randomTiles: list, year: int) -> ee.featurecollection.FeatureCollection:
-    
 
     pprint(randomTiles)
 
@@ -145,52 +152,52 @@ def trainRandomTileImg(randomTiles: list, year: int) -> ee.featurecollection.Fea
 
         grid = dfReferenceArea['gridname'].values[0]
 
-        assetRandomTile = '{}/{}-{}-{}'.format(ASSET_SAMPLES, grid, str(year),'5')
-        
+        assetRandomTile = '{}/{}-{}-{}'.format(
+            ASSET_SAMPLES, grid, str(year), '5')
+
         sp = ee.FeatureCollection(assetRandomTile).remap(
-            CLASS_REMAP[:,0:1].flatten().tolist(), 
-            CLASS_REMAP[:,1:2].flatten().tolist(),
+            CLASS_REMAP[:, 0:1].flatten().tolist(),
+            CLASS_REMAP[:, 1:2].flatten().tolist(),
             'class'
         ).select(['class'])
 
-        vals = sp.reduceColumns(ee.Reducer.frequencyHistogram(), ['class']).get('histogram').getInfo()
-
-        pprint(vals)
-
         colTile = (ee.ImageCollection('COPERNICUS/S2_HARMONIZED')
-            .filterDate(str(year) + '-01-01', str(year) + '-12-30')
-            .filter('CLOUDY_PIXEL_PERCENTAGE <= 50')
-            .filter(ee.Filter.eq('MGRS_TILE', tile))
-            .map(lambda image: 
-                image.select('B2', 'B3', 'B4', 'B8', 'B11', 'B12')\
-                    .divide(10000).addBands(image.select('QA60'))\
-                    .copyProperties(image))
-            .select(BANDS, NEW_BAND_NAMES)
-            .sort('CLOUDY_PIXEL_PERCENTAGE')
-            .map(getFractions)
-            .map(getNdfi)
-            .map(getCsfi)
-            .map(removeCloudShadow)
-            .map(removeShadow)
-        )
+                   .filterDate(str(year) + '-01-01', str(year) + '-12-30')
+                   .filter('CLOUDY_PIXEL_PERCENTAGE <= 50')
+                   .filter(ee.Filter.eq('MGRS_TILE', tile))
+                   .map(lambda image:
+                        image.select('B2', 'B3', 'B4', 'B8', 'B11', 'B12')
+                        .divide(10000).addBands(image.select('QA60'))
+                        .copyProperties(image))
+                   .select(BANDS, NEW_BAND_NAMES)
+                   .sort('CLOUDY_PIXEL_PERCENTAGE')
+                   .map(getFractions)
+                   .map(getNdfi)
+                   .map(getCsfi)
+                   .map(removeCloudShadow)
+                   .map(removeShadow)
+                )
 
         minMax = colTile.select(['ndfi', 'gv', 'soil']).reduce(ee.Reducer.minMax())
         median = colTile.select(['ndfi', 'gv', 'soil']).reduce(ee.Reducer.median())
 
-        imageTile = ee.Image(colTile.first()).addBands(minMax).addBands(median)
+        imageTile = ee.Image(colTile.first()).addBands(minMax).addBands(median)\
+            .select(FEAT_SPACE_BANDS)
 
         samplesRandomTileTrain = imageTile.sampleRegions(
-            collection=sp,  
+            collection=sp,
             scale=10
         )
 
         return samplesRandomTileTrain
 
-    listTrainRandom = map(lambda tile: trainOneImageByTile(year, tile), randomTiles)
+    listTrainRandom = map(
+        lambda tile: trainOneImageByTile(year, tile), randomTiles)
 
     trained = ee.FeatureCollection(list(listTrainRandom)).flatten()
 
     return trained
+
 
 '''
     Input Data
@@ -200,10 +207,6 @@ amazonia = ee.FeatureCollection(ASSET_BIOMES).filter('Bioma == "Amazônia"')
 
 tilesCollection = ee.FeatureCollection(ASSET_TILES)\
     .filterBounds(amazonia)
-
-alreadyInCollection = ee.ImageCollection(ASSET_OUTPUT)\
-    .reduceColumns(ee.Reducer.toList(), ['tile']).get('list')\
-    .getInfo()
 
 if len(TILES) == 0:
     TILES = tilesCollection.reduceColumns(ee.Reducer.toList(), ['NAME']).get('list')\
@@ -215,40 +218,44 @@ if len(TILES) == 0:
 
 for year in YEARS:
 
-    for tile in TILES[:1]:
+    for tile in TILES:
 
-        currentTile = ee.Feature(tilesCollection.filter(ee.Filter.eq('NAME', tile)).first())
+        alreadyInCollection = ee.ImageCollection(ASSET_OUTPUT)\
+            .filter('tile == "{}"'.format(tile))\
+            .reduceColumns(ee.Reducer.toList(), ['id_image']).get('list')\
+            .getInfo()
+
+
+        currentTile = ee.Feature(tilesCollection.filter(
+            ee.Filter.eq('NAME', tile)).first())
 
         geometry = currentTile.geometry()
-        
-        ## region = geometry.getInfo()['coordinates']
 
         collection = (ee.ImageCollection('COPERNICUS/S2_HARMONIZED')
-            .filterDate(str(year) + '-01-01', str(year) + '-12-30')
-            .filter('CLOUDY_PIXEL_PERCENTAGE <= 50')
-            .filter(ee.Filter.eq('MGRS_TILE', tile))
-            .map(lambda image: 
-                image.select('B2', 'B3', 'B4', 'B8', 'B11', 'B12')\
-                    .divide(10000).addBands(image.select('QA60'))\
-                    .copyProperties(image))
-            .select(BANDS, NEW_BAND_NAMES)
-            .map(getFractions)
-            .map(getNdfi)
-            .map(getCsfi)
-            .map(removeCloudShadow)
-            .map(removeShadow)
-        )
-        
+                      .filterDate(str(year) + '-01-01', str(year) + '-12-30')
+                      .filter('CLOUDY_PIXEL_PERCENTAGE <= 50')
+                      .filter(ee.Filter.eq('MGRS_TILE', tile))
+                      .map(lambda image:
+                           image.select('B2', 'B3', 'B4', 'B8', 'B11', 'B12')
+                           .divide(10000).addBands(image.select('QA60'))
+                           .copyProperties(image))
+                      .select(BANDS, NEW_BAND_NAMES)
+                      .map(getFractions)
+                      .map(getNdfi)
+                      .map(getCsfi)
+                      .map(removeCloudShadow)
+                      .map(removeShadow)
+                      )
+
         minMax = collection.select(['ndfi', 'gv', 'soil']).reduce(ee.Reducer.minMax())
         median = collection.select(['ndfi', 'gv', 'soil']).reduce(ee.Reducer.median())
 
         listIdImages = collection.reduceColumns(ee.Reducer.toList(), ['system:index']).get('list')\
             .getInfo()
-        
+
         listIdImages = list(set(listIdImages) - set(alreadyInCollection))
 
-        listIdImages = ['20200709T142741_20200709T142735_T21MTU']
-        for idImage in listIdImages[:1]:
+        for idImage in listIdImages:
             try:
 
                 tile = tile
@@ -266,37 +273,30 @@ for year in YEARS:
                 gridName = dfReferenceArea['gridname'].values[0]
 
                 # samples from one tile
-                assetTileSamples = '{}/{}-{}-{}'.format(ASSET_SAMPLES, gridName, str(yearSample),'5')
+                assetTileSamples = '{}/{}-{}-{}'.format(ASSET_SAMPLES, gridName, str(yearSample), '5')
                 samplesTile = ee.FeatureCollection(assetTileSamples).remap(
-                    CLASS_REMAP[:,0:1].flatten().tolist(), 
-                    CLASS_REMAP[:,1:2].flatten().tolist(),
+                    CLASS_REMAP[:, 0:1].flatten().tolist(),
+                    CLASS_REMAP[:, 1:2].flatten().tolist(),
                     'class'
                 ).select(['class'])
 
-
-
                 # train samples current tile
                 samplesTileTrain = image.sampleRegions(
-                    collection=samplesTile,  
+                    collection=samplesTile,
                     scale=10
                 )
 
                 # train samples random images
-                tilesRandom = list(random.choices(TILES, k=3))
-                samplesTilesRandom = trainRandomTileImg(tilesRandom, year)
-
-
-                # get trainning samples
-                #samplesTileFc = ee.FeatureCollection(list(dfReferenceArea['samples_gee'].values)).flatten()
-                #samplesTileFc = samplesTileFc.select(['class'])
-
+                tilesRandom = list(random.choices(TILES, k=15))
+                samplesTilesRandom = trainRandomTileImg(tilesRandom, yearSample)
 
                 # filter samples based on proportion
                 samplesToModel = ee.FeatureCollection(samplesTilesRandom.merge(samplesTileTrain))
                 samplesToModel = shuffle(collection=samplesToModel)
 
                 dfReferenceArea['samples_gee'] = dfReferenceArea.apply(
-                    lambda serie: samplesToModel.filter(ee.Filter.eq('class', serie['class'])).limit(serie['n_samples']),
+                    lambda serie: samplesToModel.filter(ee.Filter.eq(
+                        'class', serie['class'])).limit(serie['n_samples']),
                     axis=1
                 )
 
@@ -306,7 +306,7 @@ for year in YEARS:
                 # create model
                 model = ee.Classifier.smileRandomForest(**RF_PARAMS)\
                     .train(samplesToModel, 'class', FEAT_SPACE_BANDS)
-                
+
                 # predict image
                 classification = image.classify(model)
 
@@ -318,10 +318,10 @@ for year in YEARS:
                     .set('year', year)\
                     .set('id_image', idImage)\
                     .byte()
-                
-                name = '{}_{}_{}_{}_{}'.format(idImage,str(tile), gridName, str(year), OUTPUT_VERSION)
-                assetId = '{}/{}'.format(ASSET_OUTPUT, name)
 
+                name = '{}_{}_{}_{}_{}'.format(idImage, str(
+                    tile), gridName, str(year), OUTPUT_VERSION)
+                assetId = '{}/{}'.format(ASSET_OUTPUT, name)
 
                 pprint('Exporting... ' + name)
 
@@ -333,7 +333,6 @@ for year in YEARS:
                     region=geometry,
                     maxPixels=1e13
                 )
-
 
                 task.start()
 
